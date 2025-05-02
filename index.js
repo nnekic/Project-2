2ef02ec17b77b144d1c79efbb1830b5d4de665d0
Nemanja <95zakon@gmail.com>
2025-05-02 21:16:23 +0200

    Implementing /health endpoint check

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  if (req.url == '/health'){
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end (JSON.stringify({ status: "OK" }));
+    return;
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Simple Node app works!\n');
 });
+
+
 
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
